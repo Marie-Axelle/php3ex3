b397abbad79c1acf2656a53f4945ccba46320576
Marie-Axelle Simoes <Vous@exemple.com>
2017-12-13 10:43:22 +0100

    boucle 3

---
+++ index.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>php boucles</title>
+  </head>
+  <body>
+    <h1>Les chatons</h1>
+    <?php
+    $test= 100;
+    $count=45;
+
+    while ($test >= 10) {
+      # code...
+      --$test;
+      echo $test * $count;
+
+    }
+     ?>
+
+  </body>
+</html>
